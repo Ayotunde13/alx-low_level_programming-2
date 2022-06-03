0130cc89c912e6ad022f81667bb9402aff418a0f
Teiyem <teethedev@gmail.com>
2022-06-03 07:15:32 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,17 @@
 
 /**
  * main - program entry point
- * Description: Print all single digit numbers of base 10 starting from 0
+ * Description: Prints all single digit base 10 numbers starting from 0
  * Return: 0
  */
 int main(void)
 {
-	int c = 0;
+	int x = 0;
 
-	while (c < 10)
+	while (x < 10)
 	{
-		putchar(48 + c);
-		c++;
+		putchar(48 + x);
+		x++;
 	}
 
 	putchar('\n');
