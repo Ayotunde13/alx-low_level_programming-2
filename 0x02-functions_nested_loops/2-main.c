26f3a519407e52bb1881819efac706e8a5bd0743
Teiyem <teethedev@gmail.com>
2022-06-07 01:13:49 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Program entry point
+ * Description: Prints the letters of the alphabet in lowercase
+ * 10 times using a function prototype
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
