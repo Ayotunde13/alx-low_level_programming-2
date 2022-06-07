fc168dcc4100d9872e0dc0b084b07109d55abed9
Teiyem <teethedev@gmail.com>
2022-06-07 06:43:59 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,50 +9,44 @@
  */
 void print_times_table(int n)
 {
-	int r;
+	int r, c, p;
 
 	if (n < 0 || n >= 15)
 		return;
 
 	for (r = 0; r <= n; r++)
 	{
-		int c;
-
 		for (c = 0; c <= n; c++)
 		{
-			int p = r * c;
-
-			if (c != 0)
-			{
-				if (p < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(p % 10 + '0');
-				}
-				else if (p < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((p / 10) % 10 + '0');
-					_putchar(p % 10 + '0');
-				}
-				else if (p < 1000)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(p / 100 + '0');
-					_putchar((p / 10) % 10 + '0');
-					_putchar(p % 10 + '0');
-				}
-			}
-			else
+			p = r * c;
+			if (c == 0)
 				_putchar('0');
-		}
 
+			else if (p < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(p % 10 + '0');
+			}
+			else if (p < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((p / 10) % 10 + '0');
+				_putchar(p % 10 + '0');
+			}
+			else if (p < 1000)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(p / 100 + '0');
+				_putchar((p / 10) % 10 + '0');
+				_putchar(p % 10 + '0');
+			}
+		}
 		_putchar('\n');
 	}
 }
