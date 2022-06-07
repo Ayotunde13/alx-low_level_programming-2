3ce613ec36927b8a94366652e52617ba647e3b53
Teiyem <teethedev@gmail.com>
2022-06-07 11:05:18 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,36 +7,36 @@
  */
 int main(void)
 {
-            unsigned long int i;
+	unsigned long int i, j, k, j1, j2, k1, k2;
+	unsigned long int max = 1000000000;
 
-            unsigned long int j = 1;
-            unsigned long int k = 2;
+	j = 1;
+	k = 2;
 
-            printf("%lu", j);
+	printf("%lu", j);
 
-            for (i = 1; i < 91; i++)
-            {
-                printf(", %lu", k);
-                k += j;
-                j = k - j;
-            }
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", k);
+		k += j;
+		j = k - j;
+	}
 
-            unsigned long int j1 = j / 1000000000;
-            unsigned long int j2 = j % 1000000000;
-            unsigned long int k1 = k / 1000000000;
-            unsigned long int k2 = k % 1000000000;
+	j1 = j / max;
+	j2 = j % max;
+	k1 = k / max;
+	k2 = k % max;
 
-            for (i = 92; i < 99; ++i)
-            {
-                printf(", %lu", k1 + k2 / 1000000000);
-                printf("%lu", k2 % 1000000000);
-                k1 += j1;
-                j1 = k1 - j1;
-                k2 += j2;
-                j2 = k2 - j2;
-            }
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + k2 / max);
+		printf("%lu", k2 % max);
+		k1 += j1;
+		j1 = k1 - j1;
+		k2 += j2;
+		j2 = k2 - j2;
+	}
 
-            printf("\n");
-
-            return (0);
+	printf("\n");
+	return (0);
 }
