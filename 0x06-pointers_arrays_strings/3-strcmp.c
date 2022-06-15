506fdb0fde4dd2d734ebc6cd17c3401a13448bf5
Thabang Mmakgatla <94882887+Teiyem@users.noreply.github.com>
2022-06-15 13:55:39 +0200

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
+ * _strcmp - Function prototype
  * Description: Compares the difference between the strings
  * @s1: The first string to compare
  * @s2: The second string to compare
  * Return: The difference between the two strings. 0 if a match and another number if not
 */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i;
