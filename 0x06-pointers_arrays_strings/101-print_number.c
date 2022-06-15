6da762c13ec73c7083e89b2bf8f8a3dcc533259a
Teiyem <teethedev@gmail.com>
2022-06-15 09:56:03 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,6 +8,9 @@
  */
 void print_number(int n)
 {
+	int d = 0;
+	int i;
+	int x;
 
 	if (n < 0)
 	{
@@ -15,8 +18,13 @@ void print_number(int n)
 		n *= -1;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	for (i = 0; n / d > 9; i++, d *= 10)
+	{
+	}
 
-	_putchar(n % 10 + '0');
+	for (; d >= 1; n %= d, d /= 10)
+	{
+		x = n / d;
+		_putchar('0' + x);
+	}
 }
