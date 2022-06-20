f4b0d9abefd156b37417da0f29ccb1a3573774cc
Teiyem <teethedev@gmail.com>
2022-06-20 13:17:20 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strspn - Function prototype
- * Description : Locates the first occurrence in the string s of any of the bytes
- * in the string accept
+ * _strpbrk - Function prototype
+ * Description : Locates the first occurrence in the string s of any of
+ * the bytes in the string accept
  * @s: Pointer to the string to search
  * @accept: Pointer to the string that contains the characters to match
  * Return: Pointer to the byte in s that matches one of the bytes in accept
